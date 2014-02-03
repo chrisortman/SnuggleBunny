cbff75552e2972cd64c794a104aae8f9581bcbe0
Chris Ortman <chrisortman@gmail.com>
2014-02-03 13:55:55 -0600

    Add command line app for running BudgetTool
    Example application built using the library to show how it
    would be used.

---
+++ src/SnuggleBunny.App/SnuggleBunny/App/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnuggleBunny.App
+{
+    using System.IO;
+    using Activity;
+    using Budget.Analyzers;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: snugglebunny.app.exe <config_file> <transaction_file>");
+                return;
+            }
+
+            var configFile = args[0];
+            var transactionFile = args[1];
+
+            if (!File.Exists(configFile))
+            {
+                Console.WriteLine("Config file {0} does not exist",Path.GetFullPath(configFile));
+                return;
+            }
+
+            if (!File.Exists(transactionFile))
+            {
+                Console.WriteLine("Transaction file {0} does not exist", Path.GetFullPath(transactionFile));
+            }
+
+            var tool = new BudgetTool(configFile);
+            tool.AddAnalyzer(new CategorySpendingLimitAnalyzer());
+            tool.AddAnalyzer(new MonthlySpendingVersusIncomeAnalyzer());
+
+            var report = new ActivityReport();
+            report.Load(transactionFile);
+
+            var alerts = tool.Analyze(report);
+            foreach (var alert in alerts)
+            {
+                Console.WriteLine(alert.Describe());
+            }
+
+        }
+    }
+}
